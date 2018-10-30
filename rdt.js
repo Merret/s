de37ff744df4fcd707742d64a1c994e11562622c
JacobLinCool <jacoblin2011@gmail.com>
2018-10-30 18:48:12 +0800

    Create rdt.js

---
+++ rdt.js
@@ -0,0 +1,3 @@
+var u=location.href;u=u.substr(u.search("\\?")+1);var x=document.getElementById("p");if(u==location.href){x.innerHTML+=" to console page.";location.href="./console"}
+else{var b=xhr.get("https://api.myjson.com/bins/"+u);if(b=="xhr.GET Error. statue: 404"){x.innerHTML+=" to console page.";location.href="./console"}
+else{b=JSON.parse(b);x.innerHTML+=" to "+b[0];location.href=b[0]}}
